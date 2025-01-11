00ad0f2de62e64e4f85e9eb12c80e1abd98dcd79
Geetika Akki <geetika.akki@gmail.com>
2025-01-11 13:28:07 +0530

    Get All Tasks UI

---
+++ Todo_frontend/todo-ui/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ListTodoComponent from './components/ListTodoComponent';
+import HeaderComponent from './components/HeaderComponent';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <HeaderComponent />
+        <ListTodoComponent/>
+          
+             
+    </>
+  )
+}
+
+export default App
---
+++ Todo_frontend/todo-ui/src/services/TodoService.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+const BASE_URL='http://localhost:8080/api/todos';
+export const getAllTodos=()=>axios.get(BASE_URL);
