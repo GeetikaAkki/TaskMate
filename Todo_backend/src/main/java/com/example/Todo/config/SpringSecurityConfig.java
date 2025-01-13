7aa037041d0105261f8cafaf22c57dec716ad0e0
Geetika Akki <geetika.akki@gmail.com>
2025-01-13 16:17:02 +0530

    Method Level Security

--- Todo_backend/src/main/java/com/example/Todo/config/SpringSecurityConfig.java
+++ Todo_backend/src/main/java/com/example/Todo/config/SpringSecurityConfig.java
@@ -2,26 +2,47 @@ package com.example.Todo.config;
 
 import java.beans.Customizer;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
+@EnableMethodSecurity
 public class SpringSecurityConfig {
+	@Bean
+	public static PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+		
+	}
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http
 
 	    .csrf(AbstractHttpConfigurer::disable)
 
-	    .authorizeHttpRequests((authz) -> authz
+	    .authorizeHttpRequests((authz) -> {
+	    	//authz.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+	   // authz.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+	    //authz.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+	   // authz.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN","USER");
+	   // authz.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN","USER");
+	    
 
-	        .anyRequest().authenticated()
+	        authz.anyRequest().authenticated();
 
-	    )
+	    })
 
 	    .httpBasic(httpBasic -> {})
 
@@ -29,8 +50,25 @@ public class SpringSecurityConfig {
 
 	        .authenticationEntryPoint(new BasicAuthenticationEntryPoint())
 	        
-
 	    );
 		return http.build();
+	}
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails geetika=User.builder()
+		.username("geetika")
+		.password(passwordEncoder().encode("password"))
+		.roles("USER")
+		.build();
+		
+		UserDetails admin=User.builder()
+				.username("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(geetika, admin);
+		
 	}
 }
--- Todo_backend/src/main/java/com/example/Todo/controller/TodoController.java
+++ Todo_backend/src/main/java/com/example/Todo/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TodoController {
 	private TodoService todoService;
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	
 	public ResponseEntity <TodoDto>addTodo(@RequestBody TodoDto todoDto){
@@ -33,6 +35,7 @@ public class TodoController {
 		return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
 		
 	}
+	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@GetMapping("{id}")
 	public ResponseEntity<TodoDto> getTodo( @PathVariable("id") Long todoId){
 		TodoDto todoDto=todoService.getTodo(todoId);
@@ -40,23 +43,26 @@ public class TodoController {
 		
 			
 		}
+	@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	@GetMapping
 	public ResponseEntity<List<TodoDto>>getAllTodos(){
 		List <TodoDto> todos=todoService.getAllTodos();
 		return new ResponseEntity<>(todos,HttpStatus.OK);
 		
 	}
+	@PreAuthorize("hasRole('ADMIN')")
 	 @PutMapping("{id}")
 	    public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId){
 	        TodoDto updatedTodo = todoService.updateTodo(todoDto, todoId);
 	        return ResponseEntity.ok(updatedTodo);
 	    }
-		
+	  @PreAuthorize("hasRole('ADMIN')")
 	  @DeleteMapping("{id}")
 	    public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId){
 	        todoService.deleteTodo(todoId);
 	        return ResponseEntity.ok("Todo deleted successfully!.");
 	    }
+	  @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	  @PatchMapping("{id}/complete")
 	  public ResponseEntity<TodoDto> completeTodo(@PathVariable("id")Long todoId){
 		  TodoDto updatedTodo=todoService.completeTodo(todoId);
@@ -64,6 +70,7 @@ public class TodoController {
 		  
 		  
 	  }
+	  @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
 	  
 	  @PatchMapping("{id}/in-complete")
 	    public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable("id") Long todoId){
