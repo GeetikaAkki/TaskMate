b3f642193072b478240f9f0b79b45e38e5db1c6c
Geetika Akki <geetika.akki@gmail.com>
2025-01-13 19:46:52 +0530

    Defined MTM relationship

--- Todo_backend/src/main/java/com/example/Todo/entity/User.java
+++ Todo_backend/src/main/java/com/example/Todo/entity/User.java
@@ -1,11 +1,19 @@
 package com.example.Todo.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +36,9 @@ public class User {
 	private String email;
 	@Column(nullable=false, unique=true)
 	private String password;
+	@ManyToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinTable(name="users_roles", joinColumns= @JoinColumn (name="user_id", referencedColumnName="id"), inverseJoinColumns=@JoinColumn(name="role_id", referencedColumnName="id"))
+	private Set<Role>roles;
 	
 
 }
